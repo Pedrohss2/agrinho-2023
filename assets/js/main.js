330a5b11db2cc496fbc34cf92d155cd1fa8383bd
PedroH <pedro2266809ph>
2023-07-27 16:30:47 -0300

    Edita modo claro e escuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 function modoNotruno() {
-    const backgroundDark = document.body.style.background = '#000';
+    const backgroundDark = document.body.style.background = '#000000';
     let colorP = document.body.style.color = '#fff';
 }
 
 function modoClaro() {
     const backgroundLight = document.body.style.background = '#fff';
-    let colorP = document.body.style.color = '#000';  
+    let colorP = document.body.style.color = '#000';
 }
