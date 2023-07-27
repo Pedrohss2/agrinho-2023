a5eca0917e904e4d217bedcd895082d05ad42064
PedroH <pedro2266809ph>
2023-07-27 17:01:06 -0300

    Muda cor do footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 function modoNotruno() {
     const backgroundDark = document.body.style.background = '#000000';
-    let colorP = document.body.style.color = '#fff';
+    let colorPcolorParagrafoDark = document.body.style.color = '#fff';
 }
 
 function modoClaro() {
     const backgroundLight = document.body.style.background = '#fff';
-    let colorP = document.body.style.color = '#000';
+    let colorParagrafoClaro = document.body.style.color = '#000'; 
 }
